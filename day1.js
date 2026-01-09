550710b005b3e39630274d65cdf38a3a7ac29d59
Balaji <93272649+codingisf@users.noreply.github.com>
2026-01-09 21:03:32 +0530

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,78 @@
+let age = 50;
+if( age >= 18 ){
+    console.log("You are eligiable for voting..!");
+    // True block
+} else{
+    console.log("You are not eligiable for voting..!");
+    // False block
+}
+
+
+let marks = 60;
+
+if (marks >= 90) {
+  console.log("Grade A");
+}
+else if (marks >= 75) {
+  console.log("Grade B");
+}
+else {
+  console.log("Grade C");
+}
+
+
+let age = 17;
+
+let result = age >= 18 ? "Adult" : "Minor";
+//  variable  = condition ? "True" : "False";
+console.log(result);
+
+age = 1 to 100 
+
+Days 1 to 7
+
+let day = 4;
+
+switch(day){
+    case 1 :
+        console.log("Monday");
+        break;
+    case 2 :
+        console.log("Tuesday");
+         break;
+    case 3 :
+        console.log("Wednesday");
+         break;
+    case 4 :
+        console.log("Thursday");
+         break;
+    case 5 :
+        console.log("Friday");
+         break;
+    case 6 :
+        console.log("Saturday");
+         break;
+    case 7 :
+        console.log("Sunday");
+         break;
+    default:
+        console.log("Invaild day");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
