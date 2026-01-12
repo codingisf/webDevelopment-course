dd74d93ac22566535043fe89f5479d530bf4090d
Balaji <93272649+codingisf@users.noreply.github.com>
2026-01-12 20:09:38 +0530

    Create day5.js

---
+++ day5.js
@@ -0,0 +1,40 @@
+// while
+
+let i = 15;
+
+while(i<=10){
+    console.log("i = ",i);
+    i++;
+}
+
+
+
+
+// for
+
+for (let j = 10; j >=1;j--){
+    console.log('j = ',j);
+}
+
+
+
+let i = 15;
+
+do{
+    console.log("i = ",i);
+    i++;
+}while(i<=10)
+
+let fruits = ["apple","orange","banana"];
+for (let f of fruits){
+    console.log(f);
+}
+
+let dict = {
+    name:"jeo",
+    age:"20"
+}
+console.log(dict[age]);
+
+
+
