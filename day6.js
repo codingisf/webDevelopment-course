47b86d9f1c407854269218fbe1e2d94202efac49
Balaji <93272649+codingisf@users.noreply.github.com>
2026-01-13 19:58:28 +0530

    Create day6.js

---
+++ day6.js
@@ -0,0 +1,97 @@
+// Functions
+
+// function welcome(){
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("Hellow world ");
+//     console.log("------------------");
+// }
+
+
+// welcome();
+// welcome();
+// welcome();
+
+
+// function add( a=1 , b=1 ){
+//     console.log(a+b);
+    
+// }
+
+
+// add(5,5);
+
+// a
+
+
+// b
+
+
+// c    dog
+
+// add(5,7);
+
+// Return
+
+// function add(a,b){      // c
+//     return a+b;
+    
+// }
+
+// let sum = add(1,3);  //a
+
+// console.log( sum );
+
+
+
+// function dog(){
+//     console.log("Bark"); 
+// }
+
+// dog();
+
+
+// const cat = function(){
+//     console.log("Meoww");
+    
+// }
+
+// cat();
+
+
+// Arrow function
+// const cat = () =>{
+//     console.log("Meoww");
+
+// }
+
+// cat();
+
+
+
+
+// const add = (a,b) =>{
+//     console.log(a+b);
+// }
+
+
+// add(5,5);
+
+
+// const greeting = (name="user") =>{
+//     console.log("Hello",name);
+// }
+
+// greeting();
+
+
+
+
+
