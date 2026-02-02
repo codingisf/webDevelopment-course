66aaaffe2c43f18a5cd504a7bb6d4bd14d03bbf2
Balaji <93272649+codingisf@users.noreply.github.com>
2026-02-02 21:17:14 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,51 @@
+// const Myfun = () =>{
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+//     console.log("Hello from function");
+    
+// };
+
+// Myfun();
+// Myfun();
+// Myfun();
+// Myfun();
+// Myfun();
+// Myfun();
+// Myfun();
+// Myfun();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
